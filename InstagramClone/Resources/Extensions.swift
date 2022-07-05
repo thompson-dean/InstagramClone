461b59f8f74f9d22bf80ddaa31d255584c7a53b4
Dean Thompson <deanthompson.music@gmail.com>
2022-07-05 22:50:31 +0900

    Almost finished Lesson 3

---
+++ InstagramClone/Resources/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  InstagramClone
+//
+//  Created by Dean Thompson on 2022/07/05.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.y + frame.size.width
+    }
+}
